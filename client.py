616a52604098f0d3be0ee71f1b35268dc8b037b5
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-03-04 17:29:49 -0500

    Create client.py

---
+++ client.py
@@ -0,0 +1,89 @@
+import json
+import requests
+from config import load_config
+from loading import Loading
+
+print("------------------------------------------------------------------\n")
+print("                         Claude AI Toolkit                        \n")     
+print("               API Wrapper & Command-line Interface               \n")   
+print("                       [v1.0.0] by @rmncldyo                      \n")  
+print("------------------------------------------------------------------\n")
+
+class Client:
+    def __init__(self, api_key=None):
+        self.config = load_config(api_key=api_key)
+        self.api_key = api_key if api_key else self.config.get('api_key')
+        self.base_url = self.config.get('base_url')
+        self.version = self.config.get('version')
+        self.timeout = self.config.get('timeout')
+        self.headers = {
+            "anthropic-version": "2023-06-01",
+            "content-type": "application/json",
+            "x-api-key": self.api_key
+        }
+    
+    def get(self, endpoint):
+        loading = Loading()
+        url = f"{self.base_url}/{self.version}/{endpoint}"
+        try:
+            loading.start()
+            response = requests.get(url, headers=self.headers)
+            response = response.json()
+            return response
+        except Exception as e:
+            print(f"HTTP Error: {e}")
+            raise
+        finally:
+            loading.stop()
+
+    def post(self, endpoint, data):
+        loading = Loading()
+        url = f"{self.base_url}/{self.version}/{endpoint}"
+        try:
+            loading.start()
+            response = requests.post(url, headers=self.headers, json=data)
+            response = response.json()
+            try:
+                response = response["content"][0]["text"]
+                return response
+            except:
+                loading.stop()
+                print(f"\nError: {response['error']["message"]}")
+                exit(1)
+        except Exception as e:
+            print(f"HTTP Error: {e}")
+            raise
+        finally:
+            loading.stop()
+
+    def stream_post(self, endpoint, data):
+        loading = Loading()
+        url = f"{self.base_url}/{self.version}/{endpoint}"
+        full_response = []
+        try:
+            loading.start()
+            response = requests.post(url, headers=self.headers, json=data, stream=True)
+            response.raise_for_status()
+            loading.stop()
+            response_content = ""
+            print("Assistant: ", end="", flush=True)
+            for line in response.iter_lines():
+                if line and 'text_delta' in line.decode('utf-8'):
+                    try:
+                        json_line = line.decode('utf-8').split("data: ")[1]
+                        event_data = json.loads(json_line)
+                        if event_data.get("type") == "content_block_delta":
+                            content = event_data.get("delta", {}).get("text", "")
+                            print(content, end="", flush=True)
+                            response_content += content
+                    except Exception as e:
+                        print(f"JSON Error: {e}")
+                        raise
+            full_response.append(response_content)
+            print()
+            return full_response[0]
+        except Exception as e:
+            print(f"Stream HTTP Error: {e}")
+            raise
+        finally:
+            loading.stop()
