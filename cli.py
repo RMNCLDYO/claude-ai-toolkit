c0edf94d761b5200825ee8c5657f442eb0cddb00
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-03-13 20:56:34 -0400

    Update cli.py
    Update v1.0.1:
    - Added support for new model: 'Haiku'.
    - Changed CLI tag for stream mode from `-st` to `-s`.

--- cli.py
+++ cli.py
@@ -15,22 +15,22 @@ def main():
 
     Claude AI toolit is an API wrapper and command-line interface for Anthropic's latest Claude 3 large-language models.
 
-    | Option(s)             | Description                          | Example Usage                                      |
-    |-----------------------|--------------------------------------|----------------------------------------------------|
-    | -c, --chat            | Enable chat mode                     | --chat                                             |
-    | -t, --text            | Enable text mode                     | --text                                             |
-    | -v, --vision          | Enable vision mode                   | --vision                                           |
-    | -p,  --prompt         | User prompt                          | --prompt "Hello, how can I assist you today?"      |
-    | -i,  --image          | Image file path or url               | --image "path_or_url_goes_here"                    |
-    | -a,  --api_key        | Claude API key for authentication    | --api_key "api_key_goes_here"                      |
-    | -m,  --model          | The model you would like to use      | --model "model_name_goes_here"                     |
-    | -sp, --system_prompt  | System prompt (instructions)         | --system_prompt "You are an advanced AI assistant" |
-    | -mt, --max_tokens     | Maximum number of tokens to generate | --max_tokens 1024                                  |
-    | -ss, --stop_sequences | Stop sequences for completion        | --stop_sequences ["SAFTEY_WORD", "SAFTERY_WORD_2"] |
-    | -st,  --stream        | Enable streaming mode for responses  | --stream                                           |
-    | -tm,  --temperature   | Sampling temperature                 | --temperature 0.7                                  |
-    | -tp, --top_p          | Nucleus sampling threshold           | --top_p 0.9                                        |
-    | -tk, --top_k          | Top-k sampling threshold             | --top_k 40                                         |
+    | **Description**               	| **CLI Flag(s)**           	| **CLI Usage**                                         	|
+    |-------------------------------	|---------------------------	|-------------------------------------------------------	|
+    | Enable chat mode              	| `-c`,  `--chat`           	| --chat                                                	|
+    | Enable text mode              	| `-t`,  `--text`           	| --text                                                	|
+    | Enable vision mode            	| `-v`,  `--vision`         	| --vision                                              	|
+    | User prompt                   	| `-p`,  `--prompt`         	| --prompt "Craft in depth lesson plans on any subject" 	|
+    | Image file path or url        	| `-i`,  `--image`          	| --image "image_path_or_url"                           	|
+    | API key for authentication    	| `-a`,  `--api_key`        	| --api_key "your_api_key"                              	|
+    | Model to use                  	| `-m`,  `--model`          	| --model "claude-3-opus-20240229"                      	|
+    | Enable streaming mode         	| `-s`,  `--stream`         	| --stream                                              	|
+    | System prompt (instructions)  	| `-sp,  '--system_prompt`  	| --system_prompt "You are an advanced AI assistant"    	|
+    | Maximum tokens to generate    	| `-mt`, `--max_tokens`     	| --max_tokens 1024                                     	|
+    | Sampling temperature          	| `-tm`, `--temperature`    	| --temperature 0.7                                     	|
+    | Nucleus sampling threshold    	| `-tp`, `--top_p`          	| --top_p 0.9                                           	|
+    | Top-k sampling threshold      	| `-tk`, `--top_k`          	| --top_k 40                                            	|
+    | Stop sequences for completion 	| `-ss`, `--stop_sequences` 	| --stop_sequences ["\n", "."]                          	|
     """,
         formatter_class=CustomFormatter,
         epilog="For detailed usage information, visit our ReadMe here: github.com/RMNCLDYO/claude-ai-toolkit"
@@ -42,13 +42,13 @@ def main():
     parser.add_argument('-i', '--image', type=str, help='Image file path or url', metavar='')
     parser.add_argument('-a', '--api_key', type=str, help='Claude API key for authentication', metavar='')
     parser.add_argument('-m', '--model', type=str, default='claude-3-opus-20240229', help='The model you would like to use', metavar='')
+    parser.add_argument('-s', '--stream', action='store_true', help='Enable streaming mode for responses')
     parser.add_argument('-sp', '--system_prompt', type=str, help='Initial system prompt (instructions)', metavar='')
     parser.add_argument('-mt', '--max_tokens', type=int, help='Maximum number of tokens to generate', metavar='')
-    parser.add_argument('-ss', '--stop_sequences', type=str, nargs='+', help='Stop sequences for completion', metavar='')
-    parser.add_argument('-st', '--stream', action='store_true', help='Enable streaming mode for responses')
     parser.add_argument('-tm', '--temperature', type=float, help='Sampling temperature', metavar='')
     parser.add_argument('-tp', '--top_p', type=float, help='Nucleus sampling threshold', metavar='')
     parser.add_argument('-tk', '--top_k', type=int, help='Top-k sampling threshold', metavar='')
+    parser.add_argument('-ss', '--stop_sequences', type=str, nargs='+', help='Stop sequences for completion', metavar='')
 
     args = parser.parse_args()
     
